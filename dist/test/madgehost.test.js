d723f1568417e16f1659138e2b01962cc4fa2cfe
dazinator <darrell.tunnell@googlemail.com>
2020-04-03 23:27:10 +0100

    found these changes, must have been half way through then forgotten about..

--- dist/test/madgehost.test.js
+++ dist/test/madgehost.test.js
@@ -1,14 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const madgehost_1 = require("../madgehost");
+const fs = require("fs");
 const assert_1 = require("assert");
 describe("MadgeHost", () => {
     describe("Get AMD Dependencies", () => {
-        it("Returns ModuleB is dependent upon ModuleA", () => {
+        it("Uses InMemory Files", () => {
             // Arrange          
             let sut = new madgehost_1.default();
-            var options = "testfiles/AMD/ModuleB.js";
-            sut.GetDependencies(options).then(result => {
+            var fileContents = fs.readFileSync('testFiles/AMD/ModuleB.js', "utf-8");
+            // import("mock-fs").then(mockfs =>{
+            var dir = {
+                "testfiles/AMD/": {
+                    "ModuleB.js": fileContents
+                }
+            };
+            //   mockfs()
+            //  var options = "testfiles/AMD/ModuleB.js";           
+            sut.GetDependencies(dir).then(result => {
                 var obj = result.obj();
                 console.log(Object.keys(obj));
                 var moduleBdeps = obj["ModuleB.js"];
@@ -16,26 +25,46 @@ describe("MadgeHost", () => {
                 if (!moduleBdeps.includes("ModuleA.js")) {
                     assert_1.fail("error " + "did not detect that ModuleB is dependent on ModuleA.");
                 }
+                mockfs.restore();
             }).catch((reason) => {
                 assert_1.fail("error " + reason);
-            });
-        });
-        it("Module A is not dependent on anything", () => {
-            // Arrange          
-            let sut = new madgehost_1.default();
-            var options = "testfiles/AMD/ModuleA.js";
-            sut.GetDependencies(options).then(result => {
-                var obj = result.obj();
-                console.log(Object.keys(obj));
-                var deps = obj["ModuleA.js"];
-                console.log(deps);
-                if (deps.length != 0) {
-                    assert_1.fail("error " + "did not detect that ModuleA is not dependent on anything.");
-                }
-            }).catch((reason) => {
-                assert_1.fail("error " + reason);
+                // after a test runs
+                mockfs.restore();
             });
         });
     });
+    it("Returns ModuleB is dependent upon ModuleA", () => {
+        // Arrange          
+        let sut = new madgehost_1.default();
+        var options = "testfiles/AMD/ModuleB.js";
+        sut.GetDependencies(options).then(result => {
+            var obj = result.obj();
+            console.log(Object.keys(obj));
+            var moduleBdeps = obj["ModuleB.js"];
+            console.log(moduleBdeps);
+            if (!moduleBdeps.includes("ModuleA.js")) {
+                assert_1.fail("error " + "did not detect that ModuleB is dependent on ModuleA.");
+            }
+        }).catch((reason) => {
+            assert_1.fail("error " + reason);
+        });
+    });
+    it("Module A is not dependent on anything", () => {
+        // Arrange          
+        let sut = new madgehost_1.default();
+        var options = "testfiles/AMD/ModuleA.js";
+        sut.GetDependencies(options).then(result => {
+            var obj = result.obj();
+            console.log(Object.keys(obj));
+            var deps = obj["ModuleA.js"];
+            console.log(deps);
+            if (deps.length != 0) {
+                assert_1.fail("error " + "did not detect that ModuleA is not dependent on anything.");
+            }
+        }).catch((reason) => {
+            assert_1.fail("error " + reason);
+        });
+    });
 });
+;
 //# sourceMappingURL=madgehost.test.js.map
--- src/test/madgehost.test.ts
+++ src/test/madgehost.test.ts
@@ -1,10 +1,65 @@
 import madgeHost from "../madgehost";
-import * as fs from "fs";
 import { fail, AssertionError } from "assert";
+
+import {ufs as fileSystem} from 'unionfs';
+import {fs as memoryFileSystem, Volume as volume} from 'memfs';
+import * as physicalFileSystem from 'fs';
+
+const amdVolume = volume.fromJSON({
+    '/AMD/ModuleB.js': physicalFileSystem.readFileSync('testFiles/AMD/ModuleB.js', 'utf8'),
+    '/AMD/ModuleA.js': physicalFileSystem.readFileSync('testFiles/AMD/ModuleA.js', 'utf8'),
+});
+
+fileSystem.use(amdVolume)
+          .use(physicalFileSystem);
 
 describe("MadgeHost", () => {
 
     describe("Get AMD Dependencies", () => {
+
+        it("Uses InMemory Files", () => {
+
+            // Arrange          
+            let sut = new madgeHost();
+           // var fileContents = fs.readFileSync('testFiles/AMD/ModuleB.js', "utf-8");
+
+           // import("mock-fs").then(mockfs =>{
+            //   mockfs()
+
+              //  var options = "testfiles/AMD/ModuleB.js";           
+
+                sut.GetDependencies(dir).then(result => {
+                                      
+                    var obj = result.obj();
+                    console.log(Object.keys(obj));
+                   
+                    var moduleBdeps = obj["ModuleB.js"];
+                    console.log(moduleBdeps);    
+                    
+                    if(!moduleBdeps.includes("ModuleA.js"))
+                    {
+                        fail("error " + "did not detect that ModuleB is dependent on ModuleA.")
+                    }      
+                    
+                    mockfs.restore();
+    
+                }).catch((reason) => {
+                    fail("error " + reason)
+                    // after a test runs
+                    mockfs.restore();
+                });
+
+
+            });
+
+           
+
+           
+
+         
+        });   
+
+
 
         it("Returns ModuleB is dependent upon ModuleA", () => {
 
